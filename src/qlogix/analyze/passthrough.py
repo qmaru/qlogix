471acbc96a10da5117e9e338c04f03105a302998
Kami Chen <csmcc2@hotmail.com>
2026-05-24 00:19:51 +0800

    passthrough result

--- src/qlogix/analyze/passthrough.py
+++ src/qlogix/analyze/passthrough.py
@@ -1,3 +1,5 @@
+import json
+
 from qlogix.analyze.base import Analyze, AnalyzeBaseContent
 from qlogix.source.base import SourceBaseContent
 
@@ -8,4 +10,8 @@ class PassthroughContent(AnalyzeBaseContent):
 
 class PassthroughAnalyze(Analyze[PassthroughContent]):
     def run(self, events: list[SourceBaseContent]) -> PassthroughContent:
-        return PassthroughContent(events=events, result="passthrough")
+        result = [event.model_dump() for event in events]
+        return PassthroughContent(
+            events=events,
+            result=json.dumps(result, indent=2, ensure_ascii=False),
+        )
